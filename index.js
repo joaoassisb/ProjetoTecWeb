631c635bcebdb4568128543768cd616efc2f4d63
marlondias08 <39464046+marlondias08@users.noreply.github.com>
2018-10-29 22:20:38 -0300

    funcionou

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
 /* eslint no-console:0 */
 
-const server = require("./src/server");
+const express = require("express");
+const helmet = require("helmet");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+const app = express();
+
+
+app.use(helmet());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.get('/', function(req, res) {
+  res.redirect('/login.html');
+});
+
+app.use(
+  express.static("./css", {
+    index: false,
+    maxAge: "1ms"
+  })
+);
+
+app.use(
+  express.static("./html", {
+    index: false,
+    maxAge: "1ms"
+  })
+);
+
+app.use(
+  express.static("./js", {
+    index: false,
+    maxAge: "1ms"
+  })
+);
 
 const PORT = process.env.PORT || 9000;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.info(`Server listening at port ${PORT}`);
 }); 
 
