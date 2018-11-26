f33745b2e0aa5505e4744a8d253a08156e34c813
João Vitor <joaoassisb@gmail.com>
2018-11-25 23:42:16 -0200

    fix: altera rota padrão de login usuario

--- src/static/home/home.js
+++ src/static/home/home.js
@@ -1,11 +1,5 @@
-angular
-  .module("app")
-  .config($routeProvider => {
-    $routeProvider.when("/home", {
-      template: "<home>"
-    });
-  })
-  .component("home", {
-    templateUrl: "home.html",
-    controller() {}
+angular.module("app").config($routeProvider => {
+  $routeProvider.when("/home", {
+    template: "<perfil-usuario>"
   });
+});
